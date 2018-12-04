e49568956a693d78226c1ff698c36a3ba66ba65d
Aly <alyabderrahmanefall@gmail.com>
2018-12-04 18:03:01 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/*#include<conio.h>*/
+#include<stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Format : programme <port> <...> \n");
+		exit(EXIT_FAILURE);
+	}
+	
+
+	return 0;
+}
